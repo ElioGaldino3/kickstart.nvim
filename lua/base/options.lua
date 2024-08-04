354db64942c35ea92cbe273cad6d5e7fad226f7b
Elio Galdino <eliogaldino79@outlook.com>
2024-08-04 19:08:10 -0300

    atualizando...

--- lua/base/options.lua
+++ lua/base/options.lua
@@ -37,3 +37,30 @@ vim.opt.hlsearch = true
 
 -- Obsidian config
 vim.opt.conceallevel = 2
+
+-- Neovide Configuration
+
+vim.g.neovide_fullscreen = false
+vim.g.neovide_floating_blur_amount_x = 0.0
+vim.g.neovide_floating_blur_amount_y = 0.0
+vim.g.floaterm_winblend = 15
+vim.g.neovide_remember_window_size = true
+vim.g.neovide_padding_top = 3
+vim.g.neovide_window_blurred = false
+vim.g.neovide_floating_shadow = false
+vim.g.neovide_transparency = 0.8
+vim.g.neovide_show_border = false
+vim.g.neovide_scroll_animation_far_lines = 1
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_theme = 'auto'
+vim.g.neovide_padding_bottom = 3
+vim.g.neovide_padding_right = 3
+vim.g.neovide_padding_left = 3
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_trail_length = 0
+vim.g.neovide_cursor_antialiasing = false
+vim.g.neovide_cursor_animate_command_line = false
+vim.g.neovide_cursor_smooth_blink = false
+vim.opt.linespace = 0
+vim.g.neovide_cursor_vfx_mode = ''
+vim.o.guifont = 'Hasklug Nerd Font:h12' -- text below applies for VimScript
